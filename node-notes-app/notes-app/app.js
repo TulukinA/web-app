c19e8b04f361f0d15ccc1fba9f53f7ee57defe9c
A.G. Tulukin <a.g.tulukin@AGs-MacBook-Pro.fios-router.home>
2020-06-05 12:20:05 -0400

    node-notes-app

---
+++ node-notes-app/notes-app/app.js
@@ -0,0 +1,66 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+//*notes is an object with (3) properties
+const notes = require('./notes')
+
+//customize yargs version 
+yargs.version('1.1.0')
+
+// create add command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+
+        },
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// Create list command 
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Create read command 
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler() {
+        console.log('Reading a note')
+    }
+})
+
+// console.log(yargs.argv)
+yargs.parse()
