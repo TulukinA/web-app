695c01e38890d4ef84a64395d67e5cbfecd16f99
A.G. Tulukin <a.g.tulukin@AGs-MacBook-Pro.fios-router.home>
2020-07-20 15:47:48 -0400

    weather app comleted

---
+++ weather-app/weather-app/weather-app/app.js
@@ -0,0 +1,26 @@
+const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const address = process.argv[2]
+if (!address) {
+  console.log('Please provide an address')
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error)
+    }
+  
+    forecast( data.latitude, data.longtitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+      
+      console.log(data.location)
+      console.log(forecastData)
+    })  
+  })
+}
+
+
+
